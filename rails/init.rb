bcc9ba53b6360e0db05e46144259a132680a0e2b
Jay Zeschin <jay.zeschin@factorylabs.com>
2009-06-09 23:08:37 -0600

    Fixed hooks into Rails.cache

--- rails/init.rb
+++ rails/init.rb
@@ -1,5 +1,18 @@
 require 'cache_money'
 
+module ::ActiveSupport
+  module Cache
+    class CacheMoneyMemCacheStore < MemCacheStore
+      
+      def initialize
+        @addresses = $memcache.servers
+        @data = $memcache
+      end
+      
+    end
+  end
+end
+
 memcache_config = YAML.load_file(File.join(Rails.root,'config','memcached.yml'))
 
 memcache_options = memcache_config['defaults'].merge(memcache_config[Rails.env])
@@ -11,7 +24,9 @@ else
   $memcache = Cash::Mock.new
 end
 
+ActionController::Base.cache_store = :cache_money_mem_cache_store
 ActionController::Base.session_options[:cache] = $memcache if memcache_options['sessions']
+silence_warnings { Object.const_set "RAILS_CACHE", ActiveSupport::Cache.lookup_store(:cache_money_mem_cache_store) }
   
 $local = Cash::Local.new($memcache)
 $lock = Cash::Lock.new($memcache)
