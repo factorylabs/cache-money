03e4e485e757540bc2e8a91e1a40cf382b1467d1
Jay Zeschin <jay.zeschin@factorylabs.com>
2009-06-09 23:39:32 -0600

    Merged in Gabe's symbolize_keys changes

--- rails/init.rb
+++ rails/init.rb
@@ -16,16 +16,17 @@ end
 memcache_config = YAML.load_file(File.join(Rails.root,'config','memcached.yml'))
 
 memcache_options = memcache_config['defaults'].merge(memcache_config[Rails.env])
+memcache_options.symbolize_keys!
 
-unless memcache_options['disabled'] || Rails.env.test?
+unless memcache_options[:disabled] || Rails.env.test?
   $memcache = MemCache.new(memcache_options)
-  $memcache.servers = memcache_options['servers']
+  $memcache.servers = memcache_options[:servers]
 else
   $memcache = Cash::Mock.new
 end
 
 ActionController::Base.cache_store = :cache_money_mem_cache_store
-ActionController::Base.session_options[:cache] = $memcache if memcache_options['sessions']
+ActionController::Base.session_options[:cache] = $memcache if memcache_options[:sessions]
 silence_warnings { Object.const_set "RAILS_CACHE", ActiveSupport::Cache.lookup_store(:cache_money_mem_cache_store) }
   
 $local = Cash::Local.new($memcache)
